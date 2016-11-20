a745d0ab9d839a8a74ff72eb231dc388a4585317
Seiya Tokui <beam.web@gmail.com>
2016-11-20 11:05:23 +0900

    Add training function to sbn namespace

--- sbn/__init__.py
+++ sbn/__init__.py
@@ -2,4 +2,5 @@ from sbn import datasets, estimators, links, models
 from sbn.variational_model import VariationalModel
 from sbn.grad_estimator import GradientEstimator
 from sbn.random_variable import ConstantVariable, RandomVariable, SigmoidBernoulliVariable
+from sbn.training import train_variational_model
 from sbn.util import Array

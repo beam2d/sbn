b89b7ff4a7614b9a4993612e22a152c1e1331102
Seiya Tokui <beam.web@gmail.com>
2017-01-12 11:41:23 +0900

    Make settings configurable

--- train_many.py
+++ train_many.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python
 from argparse import ArgumentParser
-from queue import Empty, Queue
+from itertools import product
 import multiprocessing as mp
+from queue import Empty, Queue
 from string import Template
 from threading import Thread
-from typing import Any, Dict
+from typing import Any, Dict, List, Tuple
+
+import yaml
 
 from sbn import train_variational_model
 
 
 def main():
     parser = ArgumentParser(description='Search hyperparameter')
-    parser.add_argument('config', help='Path to the config YAML template')
+    parser.add_argument('config', help='Path to the variable config YAML')
     parser.add_argument('--gpu', '-g', default='0', help='GPU devices (comma separated)')
     args = parser.parse_args()
 
@@ -19,11 +22,13 @@ def main():
 
     devices = [int(s) for s in args.gpu.split(',')]
 
+    template_path, settings = _read_var_config(args.config)
+
     setting_queue = Queue()
-    for setting in _SETTINGS:
+    for setting in settings:
         setting_queue.put(setting)
 
-    threads = [Thread(target=_run_experiments_on_a_gpu, args=(args.config, device, setting_queue))
+    threads = [Thread(target=_run_experiments_on_a_gpu, args=(template_path, device, setting_queue))
                for device in devices]
     for thread in threads:
         thread.start()
@@ -31,19 +36,27 @@ def main():
         thread.join()
 
 
-_SETTINGS = [
-    {'batch_size': 100, 'decay': 0.001, 'lr': 1e-3, 'n_samples': 1},
-    {'batch_size': 100, 'decay': 0.001, 'lr': 3e-3, 'n_samples': 1},
-    {'batch_size': 100, 'decay': 0.001, 'lr': 1e-2, 'n_samples': 1},
-    {'batch_size': 100, 'decay': 0.001, 'lr': 3e-2, 'n_samples': 1},
-    {'batch_size': 100, 'decay': 0.001, 'lr': 1e-3, 'n_samples': 400},
-    {'batch_size': 100, 'decay': 0.001, 'lr': 3e-3, 'n_samples': 400},
-    {'batch_size': 100, 'decay': 0.001, 'lr': 1e-2, 'n_samples': 400},
-    {'batch_size': 100, 'decay': 0.001, 'lr': 3e-2, 'n_samples': 400},
-]
+def _read_var_config(path: str) -> Tuple[str, List[Dict[str, Any]]]:
+    with open(path) as f:
+        setting_all = yaml.load(f.read())
+    template_path = setting_all.pop('template')
+    settings = _dict_product(setting_all)
+    return template_path, settings
+
+
+def _dict_product(d: Dict[str, List[Any]]) -> List[Dict[str, Any]]:
+    # borrowed from maf: https://github.com/pfi/maf/blob/master/maflib/util.py
+    keys = sorted(d)
+    values = [d[key] for key in keys]
+    values_product = product(*values)
+    return [dict(zip(keys, vals)) for vals in values_product]
 
 
 def _run_experiments_on_a_gpu(template_path: str, device: int, setting_queue: Queue) -> None:
+    with open(template_path) as f:
+        template_str = f.read()
+    template = Template(template_str)
+
     while True:
         try:
             setting = setting_queue.get()
@@ -51,20 +64,18 @@ def _run_experiments_on_a_gpu(template_path: str, device: int, setting_queue: Qu
             print('device {}: quit thread'.format(device))
             break
 
+        setting_str = {k: str(v) for k, v in setting.items()}
+        config = template.substitute(setting_str)
+
         print('device {}: executing {}'.format(device, setting))
-        process = mp.Process(target=_run_experiment, args=(template_path, device, setting))
+        process = mp.Process(target=_run_experiment, args=(config, device))
         process.daemon = True
         process.start()
         process.join()
         print('device {}: finished'.format(device))
 
 
-def _run_experiment(template_path: str, device: int, setting: Dict[str, Any]):
-    with open(template_path) as f:
-        template_str = f.read()
-    template = Template(template_str)
-    setting_str = {k: str(v) for k, v in setting.items()}
-    config = template.substitute(setting_str)
+def _run_experiment(config: str, device: int):
     train_variational_model(config, device, resume='', verbose=False)
 
 
